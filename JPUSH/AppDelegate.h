d37694ebfcc540e47e2831afdce9364793600591
lzLiu <lzLiu@liulinzhedeMacBook-Pro.local>
2017-03-19 18:27:32 +0800

    Initial Commit

---
+++ JPUSH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JPUSH
+//
+//  Created by 柳麟喆 on 2017/3/19.
+//  Copyright © 2017年 lzLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JPUSH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JPUSH
+//
+//  Created by 柳麟喆 on 2017/3/19.
+//  Copyright © 2017年 lzLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
